e7a11cb877dab1455096f74798276a735b339e42
kitty365 <132466863+kitty365@users.noreply.github.com>
2025-01-08 01:31:03 +0100

    notfall

--- src/routes/lecturers/+page.svelte
+++ src/routes/lecturers/+page.svelte
@@ -10,8 +10,8 @@
 
 <div class="row mt-3">
   {#each data.lecturers as lecturer}
-    <div class="col-sm-6 col-md-4 col-lg-3 mb-2 gx-2">
-      <LecturerCard {lecturer}></LecturerCard>
+  <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
+    <LecturerCard {lecturer}></LecturerCard>
     </div>
   {/each}
 </div>
